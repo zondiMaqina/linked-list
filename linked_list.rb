aa2e5b9c201543bec54bf7bead0c3a0dff5beabd
zondiMaqina <158191395+zondiMaqina@users.noreply.github.com>
2024-10-11 12:19:01 +0000

    add tail and prepend method

--- linked_list.rb
+++ linked_list.rb
@@ -21,10 +21,42 @@ class LinkedList
       @head = new_node
     else
      current_node = @head
-     while current_node.next != nil # runs if head was already given pointed to the next node
-       current_node = current_node.next # changes var to next node
+     while current_node.next_node != nil # runs if head was already given pointed to the next node
+       current_node = current_node.next_node # changes var to next node
      end
-      current_node.next = new_node # last added node is the new node instatiated
+      current_node.next_node = new_node # last added node is the new node instatiated
+    end
+  end
+
+  def prepend(data)
+    new_node = Node.new(data)
+    if is_empty?
+      @head = new_node
+    else
+      current_node = @head
+      while current_node.next != nil
+        current_node = current_node.next_node
+      end
+      current_node.next_node = new_node
+      @head
+    end
+  end
+
+  def tail
+    if is_empty?
+      return nil
+    else
+      current_node = @head
+      while current_node.next_node != nil
+       current_node = current_node.next_node
+     end
+     current_node
     end
   end
 end
+
+list = LinkedList.new
+list.append("one")
+list.append("two")
+list.append("three")
+p list.tail
