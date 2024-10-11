831d8eb879858e48cbc8067620f08fc635fb312a
zondiMaqina <158191395+zondiMaqina@users.noreply.github.com>
2024-10-11 12:08:06 +0000

    add method for appending node to list

--- linked_list.rb
+++ linked_list.rb
@@ -5,3 +5,26 @@ class Node
     @next_node = next_node
   end
 end
+
+class LinkedList
+  def is_empty?
+    if defined?(@head) == nil # returns nil if var is undefined, then matches the statement
+      return true
+    else
+      return false
+    end
+  end
+
+  def append(data)
+    new_node = Node.new(data)
+    if self.is_empty?
+      @head = new_node
+    else
+     current_node = @head
+     while current_node.next != nil # runs if head was already given pointed to the next node
+       current_node = current_node.next # changes var to next node
+     end
+      current_node.next = new_node # last added node is the new node instatiated
+    end
+  end
+end
